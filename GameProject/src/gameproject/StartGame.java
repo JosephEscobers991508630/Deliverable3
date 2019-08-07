c4e0760c721909c8c8a6dbbb358f6151e2952d2c
escober <50589516+escober@users.noreply.github.com>
2019-08-07 01:53:10 -0400

    Update and rename GameProject.java to StartGame.java

--- GameProject/src/gameproject/StartGame.java
+++ GameProject/src/gameproject/StartGame.java
@@ -1,14 +1,17 @@
 package gameproject;
+
 /**
  * Joseph Escober
+ * Java Project - Deliverable 3 - Card Game
+ * SYST17796
  */
-public class GameProject
-{
+
+public class StartGame {
    /**
     * @param args the command line arguments
     */
-   public static void main (String[] args)
-   {
+   
+   public static void main (String[] args) {
 
       CardGame Start_Game = new CardGame();
       Start_Game.StartGame();
