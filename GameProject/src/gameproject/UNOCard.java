d07443bd91e01009e2c2ac3c5f11c3c970a9e7ec
escober <50589516+escober@users.noreply.github.com>
2019-08-07 01:54:22 -0400

    Update UNOCard.java

--- GameProject/src/gameproject/UNOCard.java
+++ GameProject/src/gameproject/UNOCard.java
@@ -2,40 +2,38 @@ package gameproject;
 
 /**
  * Joseph Escober
+ * Java Project - Deliverable 3 - Card Game
+ * SYST17796
  */
-public class UNOCard
-{
+
+public class UNOCard {
 
    CardFaceValue cardFaceValue;
    Color color;
 
 
-   public UNOCard (CardFaceValue cardFaceValue, Color color)
-   {
+   public UNOCard (CardFaceValue cardFaceValue, Color color) {
       this.cardFaceValue = cardFaceValue;
       this.color = color;
 
    }
 
-   public boolean startPlay (UNOCard oneCard)
-   {
+   public boolean startPlay (UNOCard oneCard) {
 
       return this.cardFaceValue == oneCard.cardFaceValue || this.color == oneCard.color;
    }
 
-   public boolean startPlayColor (UNOCard oneCard)
-   {
+   public boolean startPlayColor (UNOCard oneCard) {
 
       return this.color == oneCard.color;
    }
 
-   public String toString ()
-   {
+   public String toString () {
       if (cardFaceValue.equals(CardFaceValue.WILD) || cardFaceValue.equals(CardFaceValue.WILD_FOUR)) {
-         return cardFaceValue.name();
+           return cardFaceValue.name();
       }
       else {
-         return cardFaceValue.name() + "-" + color.name();
+            return cardFaceValue.name() + "-" + color.name();
       }
    }
 
