9377eb264029e7a0ebc41f0f502e14d90e62d63a
escober <50589516+escober@users.noreply.github.com>
2019-08-07 01:51:50 -0400

    Update Color.java

--- GameProject/src/gameproject/Color.java
+++ GameProject/src/gameproject/Color.java
@@ -1,10 +1,10 @@
 package gameproject;
-
 /**
  * Joseph Escober
+ * Java Project - Deliverable 3 - Card Game
+ * SYST17796
  */
 
-public enum Color
-{
+public enum Color {
    BLUE, GREEN, RED, YELLOW;
 }
