11ae46beea304b1a66d9cf04be508373b74386f0
escober <50589516+escober@users.noreply.github.com>
2019-08-07 01:32:42 -0400

    Update CardFaceValue.java

--- GameProject/src/gameproject/CardFaceValue.java
+++ GameProject/src/gameproject/CardFaceValue.java
@@ -2,10 +2,12 @@ package gameproject;
 
 /**
  * Joseph Escober
+ * Java Project - Deliverable 3 - Card Game
+ * SYST17796
  */
 
-public enum CardFaceValue
-{
+public enum CardFaceValue {
+   
    ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT,
    NINE, PICK_TWO, REVERSE, SKIP, WILD, WILD_FOUR;
 }
