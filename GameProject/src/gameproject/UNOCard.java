90af4b8aabbd7a02ceef516103058d00dfaa8d4a
escober <escober@sheridancollege.ca>
2019-08-07 23:27:00 -0400

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ GameProject/src/gameproject/UNOCard.java
@@ -0,0 +1,46 @@
+package gameproject;
+
+/**
+ * Joseph Escober
+ * Java Project - Card Game
+ * SYST17796 - Group 6
+ */
+
+public class UNOCard
+{
+
+   CardFaceValue cardFaceValue;
+   Color color;
+
+
+   public UNOCard (CardFaceValue cardFaceValue, Color color)
+   {
+      this.cardFaceValue = cardFaceValue;
+      this.color = color;
+
+   }
+
+   public boolean startPlay (UNOCard oneCard)
+   {
+
+      return this.cardFaceValue == oneCard.cardFaceValue || this.color == oneCard.color;
+   }
+
+   public boolean startPlayColor (UNOCard oneCard)
+   {
+
+      return this.color == oneCard.color;
+   }
+
+   public String toString ()
+   {
+
+      if (cardFaceValue.equals(CardFaceValue.WILD) || cardFaceValue.equals(CardFaceValue.WILD_FOUR)) {
+         return cardFaceValue.name();
+      }
+      else {
+         return cardFaceValue.name() + "--" + color.name();
+      }
+   }
+
+}
