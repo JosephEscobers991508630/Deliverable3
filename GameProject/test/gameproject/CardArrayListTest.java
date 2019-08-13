a963c6894f573b2c2223e9fd99f556aba927f683
escober <50589516+escober@users.noreply.github.com>
2019-08-12 20:31:22 -0400

    Update CardArrayListTest.java

--- GameProject/test/gameproject/CardArrayListTest.java
+++ GameProject/test/gameproject/CardArrayListTest.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package gameproject;
 
 import java.util.List;
@@ -15,6 +16,7 @@ import org.junit.Test;
 /**
  *
  * @author joseph
+ * Group 6
  */
 public class CardArrayListTest
 {
